181365ad4e53c09901a42a3ab170e424ea37550a
Aman11b <amansingh1100b@gmail.com>
2025-10-30 07:24:04 +0530

    Features section HTML completed

--- src/js/main.js
+++ src/js/main.js
@@ -13,3 +13,21 @@ btn.addEventListener("click", () => {
   logoMobile.classList.toggle("hidden");
   nav.classList.toggle("hidden");
 });
+
+const featureContent = [
+  {
+    id: 0,
+    h2: "Bookmark in one click",
+    p: "Organize your bookmarks however you like. Our simple drag-and-drop interface gives you complete control over how you manage your favourite sites.",
+  },
+  {
+    id: 1,
+    h2: "Intelligent search",
+    p: "Our powerful search feature will help you find saved sites in no time at all. No need to trawl through all of your bookmarks. ",
+  },
+  {
+    id: 2,
+    h2: "Share your bookmarks",
+    p: "Easily share your bookmarks and collections with others. Create a shareable link that you can send at the click of a button.",
+  },
+];
